63ae656813c542cc70286113b59a6c1ef5b7ab6e
Mushroomboss <mnchu007@hotmail.com>
2025-05-20 19:18:19 -0500

    prompt template

---
+++ frotune_master_blind_chen_master.py
@@ -0,0 +1,10 @@
+
+from langchain.prompts import AIMessagePromptTemplate
+from langchain.prompts import SystemMessagePromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate
+from langchain.prompts import ChatMessagePromptTemplate # customize the role and template
+
+prompt = "May {subject} be with you!"
+
+chat_message_prompt = ChatMessagePromptTemplate.from_template(role="Skywalker",template=prompt)
+chat_message_prompt.format(subject="the Force")
